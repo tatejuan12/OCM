0390bafb09805f441be0eecbb0ef4697c7d262a7
tatejuan12 <tatejuan11@gmail.com>
2022-03-23 13:00:34 +1100

    A

---
+++ assets/js/xummSDK.js
@@ -0,0 +1,11 @@
+const { XummSdk } = require("xumm-sdk");
+const Sdk = new XummSdk(
+  "d4d77457-1f7f-4c34-9665-6b8aa683ac2d",
+  "2bce5e29-6550-4c7a-abf4-9b19cb398cbe"
+);
+
+const main = async () => {
+  console.log(`Hi! This is where we'll be writing some code`);
+};
+
+main();
