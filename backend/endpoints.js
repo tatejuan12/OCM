0e751d6694d0fd6d72e3026488f5616885715ac6
tatejuan12 <tatejuan11@gmail.com>
2022-04-08 21:24:14 +1000

    removed endpoints and implementing redirects to pages

--- backend/endpoints.js
+++
@@ -1,6 +0,0 @@
-// import wildcard from "@wildcard/server";
-// async function test() {
-//   return "niggaa";
-// }
-// const endpoints = { test };
-// Object.assign(wildcard.endpoints, endpoints);
