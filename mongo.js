842d66700c073aed1094467f5fc036aeb8631bf1
tatejuan <juan.tate@cosmosaus.com>
2023-05-07 04:15:05 +0000

    connection pooling errors

--- mongo.js
+++ mongo.js
@@ -5,7 +5,34 @@ const { resourceLimits } = require("worker_threads");
 const { XummSdk } = require("xumm-sdk");
 const { getDb } = require('./mongo-utils');
 
-const mongoClient = require("mongodb").MongoClient;
+const { MongoClient } = require('mongodb');
+
+const uri = process.env.MONGO_URI;
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+let client;
+let clientPromise;
+
+// if (process.env.EXPRESS_ENV === 'development') {
+//   if (!global._mongoClientPromise) {
+//     client = new MongoClient(uri, options);
+//     global._mongoClientPromise = client.connect();
+//   }
+//   clientPromise = global._mongoClientPromise;
+// } else {
+//   client = new MongoClient(uri, options);
+//   clientPromise = client.connect();
+// }
+
+// exports.getDb = async (dbName) => {
+//   const client = await clientPromise;
+//   return client.db(dbName);
+// };
+
+MongoClient.connect(uri, options, function (err, db) {
+  assert.equal(null, err);
+  mongobd=db;
+})
 
 var methods = {
   signinHandler: async function (id) {
