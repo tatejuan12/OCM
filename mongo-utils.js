63dd45b335de5c024816b8c84846790240adbfce
tatejuan <juan.tate@cosmosaus.com>
2023-05-07 04:14:49 +0000

    connection pooling errors

--- mongo-utils.js
+++ mongo-utils.js
@@ -6,18 +6,23 @@ const options = { useNewUrlParser: true, useUnifiedTopology: true };
 let client;
 let clientPromise;
 
-if (process.env.EXPRESS_ENV === 'development') {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
-  }
-  clientPromise = global._mongoClientPromise;
-} else {
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
-}
+// if (process.env.EXPRESS_ENV === 'development') {
+//   if (!global._mongoClientPromise) {
+//     client = new MongoClient(uri, options);
+//     global._mongoClientPromise = client.connect();
+//   }
+//   clientPromise = global._mongoClientPromise;
+// } else {
+//   client = new MongoClient(uri, options);
+//   clientPromise = client.connect();
+// }
 
-exports.getDb = async (dbName) => {
-  const client = await clientPromise;
-  return client.db(dbName);
-};
+// exports.getDb = async (dbName) => {
+//   const client = await clientPromise;
+//   return client.db(dbName);
+// };
+
+MongoClient.connect(uri, options, function (err, db) {
+  assert.equal(null, err);
+  mongobd=db;
+})
